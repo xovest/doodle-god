f198f8ff265ab2b944b6787cd8642f912db7123f
xovest <79008917+xovest@users.noreply.github.com>
2022-07-20 23:34:37 +0300

    reading files is fun

---
+++ dynamic.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdlib>
+int main(){
+std::cout << "Hello World!!\
+";;
+return EXIT_SUCCESS;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "header.h"
+
+int main() {
+  string word = "Node *clothes();";
+  word = word.substr(6);
+  cout << word;
+  Node* root = clothes();
+  // cout << maxDepth(root);
+  // print2D(firearm());
+  return 0;
+}
